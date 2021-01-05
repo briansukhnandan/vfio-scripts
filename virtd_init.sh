4b27048abb1eac1918a9eb739d9b195e08d0a5ed
Brian <briansukhnandan@gmail.com>
2021-01-05 02:55:14 -0500

    added pulse to virtd_init

--- virtd_init.sh
+++ virtd_init.sh
@@ -4,6 +4,9 @@ sudo systemctl start libvirtd.service
 sudo systemctl start virtlogd.socket
 sudo cpupower -c 1-3 frequency-set -g schedutil
 
+cd /home/prophet/Documents/scream/Receivers/unix/build
+./scream -o pulse -u -p 4011 -i enp3s0
+
 
 
 
