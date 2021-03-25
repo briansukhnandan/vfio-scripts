370ba0b86784dbf54d313b4932fa8b801839b162
Brian S <briansukhnandan@gmail.com>
2021-03-24 22:50:26 -0400

    updated xml

--- virtd_init.sh
+++ virtd_init.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
 # Start libvirtd and virtlogd if not started already.
-sudo systemctl start libvirtd.service
-sudo systemctl start virtlogd.socket
-
-# Let cores 2-4 run under schedutil (cores will be passed.)
-sudo cpupower -c 1-3 frequency-set -g schedutil
+sudo modprobe vendor-reset
 
 # Now start scream server from hypervisor.
-cd /home/prophet/Documents/scream/Receivers/unix/build
-./scream -o pulse -u -p 4011 -i enp3s0
+# cd /home/prophet/Documents/scream/Receivers/unix/build
+# ./scream -o pulse -u -p 4011 -i enp3s0
 
 
 
